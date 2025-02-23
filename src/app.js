644f46e838315a2d530ca2f62bd6f25ef261d0df
Trickkyyy <vandark5@gmail.com>
2025-02-23 19:24:55 +0200

    updating the README file and adding securtity headers

--- src/app.js
+++ src/app.js
@@ -1,10 +1,39 @@
 const express = require("express");
 const cors = require("cors");
 const currencyRoutes = require("./routes/currency.routes");
-// comment
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+
 const app = express();
 
-app.use(cors());
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: "Too many requests, please try again later.",
+});
+
+app.use(
+  helmet(
+    helmet({
+      contentSecurityPolicy: false,
+      referrerPolicy: { policy: "no-referrer" },
+    })
+  )
+);
+app.disable("x-powered-by");
+
+app.use(
+  cors({
+    origin: "https://currency-converter-softteco.netlify.app",
+    methods: ["GET", "POST"],
+  })
+);
+app.use(limiter);
+app.use(express.static("public", { dotfiles: "deny" }));
+app.use((req, res, next) => {
+  if (!req.secure) return res.redirect("https://" + req.headers.host + req.url);
+  next();
+});
 app.use(express.json());
 
 app.use("/api/currency", currencyRoutes);
